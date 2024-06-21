d32988220d619423e6b1aca4274a5ad95d6c4216
Florent BEAUCHAMP <florent.beauchamp@vates.fr>
2024-06-21 11:32:37 +0200

    feat(backups): enable Change Block Tracking Backups (#7750)
    Enable CBT for all backups
    
    * enable cbt each time a VM should be backuped by nbd
    * if there are no rolling snapshot on a job =>
    the snapshots data are only kept during the transfer.
    Snapshot data deletion is behing a config flag 'purgeSnapshotData' (default false )
    
    adverse effect:
    
    * rolling back to previous XO version will make full backups
    * removed read ahead in nbd , performance gain was low with concurrency
    and it make code more complex

--- packages/vhd-lib/Vhd/VhdAbstract.js
+++ packages/vhd-lib/Vhd/VhdAbstract.js
@@ -258,8 +258,9 @@ exports.VhdAbstract = class VhdAbstract {
     assert.strictEqual(fileSize % SECTOR_SIZE, 0)
     return fileSize
   }
-
-  stream() {
+  // progress is called with currentBlock, numberOfBlocs
+  // it's an approximation, ignoring the footer/header/bat size
+  stream({ onProgress } = {}) {
     const { footer, batSize } = this
     const { ...header } = this.header // copy since we don't ant to modifiy the current header
     const rawFooter = fuFooter.pack(footer)
@@ -282,10 +283,11 @@ exports.VhdAbstract = class VhdAbstract {
     const rawHeader = fuHeader.pack(header)
     checksumStruct(rawHeader, fuHeader)
 
-    assert.strictEqual(offset % SECTOR_SIZE, 0, `offset should be aligned to SECTORE SIZE : ${offset} `)
+    assert.strictEqual(offset % SECTOR_SIZE, 0, `offset should be aligned to SECTOR_SIZE: ${offset}`)
 
     const bat = Buffer.allocUnsafe(batSize)
     let offsetSector = offset / SECTOR_SIZE
+    let nbBlocks = 0
     const blockSizeInSectors = this.fullBlockSize / SECTOR_SIZE
     let fileSize = offsetSector * SECTOR_SIZE + FOOTER_SIZE /* the footer at the end */
     // compute BAT , blocks starts after parent locator entries
@@ -294,6 +296,7 @@ exports.VhdAbstract = class VhdAbstract {
         bat.writeUInt32BE(offsetSector, i * 4)
         offsetSector += blockSizeInSectors
         fileSize += this.fullBlockSize
+        nbBlocks++
       } else {
         bat.writeUInt32BE(BLOCK_UNUSED, i * 4)
       }
@@ -302,15 +305,15 @@ exports.VhdAbstract = class VhdAbstract {
     assert.strictEqual(offset % SECTOR_SIZE, 0)
     const self = this
     let yielded = 0
-    function* trackAndYield(buffer) {
+    function trackTransmittedLength(buffer) {
       yielded += buffer.length
       assert.ok(yielded <= fileSize, `Max stream length is ${fileSize}, try to send ${yielded}`)
-      yield buffer
+      return buffer
     }
     async function* iterator() {
-      yield* trackAndYield(rawFooter)
-      yield* trackAndYield(rawHeader)
-      yield* trackAndYield(bat)
+      yield trackTransmittedLength(rawFooter)
+      yield trackTransmittedLength(rawHeader)
+      yield trackTransmittedLength(bat)
 
       // yield parent locator
 
@@ -321,21 +324,24 @@ exports.VhdAbstract = class VhdAbstract {
           // align data to a sector
           const buffer = Buffer.alloc(space, 0)
           data.copy(buffer)
-          yield* trackAndYield(buffer)
+          yield trackTransmittedLength(buffer)
         }
       }
 
       // yield all blocks
       // since contains() can be costly for synthetic vhd, use the computed bat
+      let nbYielded = 0
       for (let i = 0; i < header.maxTableEntries; i++) {
         if (bat.readUInt32BE(i * 4) !== BLOCK_UNUSED) {
+          nbYielded++
           const block = await self.readBlock(i)
-          yield* trackAndYield(block.buffer)
+          yield trackTransmittedLength(block.buffer)
+          onProgress?.(nbYielded, nbBlocks)
         }
       }
       // yield footer again
-      yield* trackAndYield(rawFooter)
-      assert.strictEqual(yielded,fileSize, `Calclulated stream length is  ${fileSize}, sent ${yielded} bytes`)
+      yield trackTransmittedLength(rawFooter)
+      assert.strictEqual(yielded, fileSize, `computed stream length is ${fileSize} but sent ${yielded} bytes`)
     }
 
     const stream = asyncIteratorToStream(iterator())
