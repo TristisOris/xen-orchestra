172244828c3726303e7625d601e8741c2f37ddf6
Olivier Floch <olivier.floch@vates.fr>
2024-07-03 10:44:57 +0200

    feat(xo-core): add CellText component

---
+++ @xen-orchestra/web-core/lib/components/cell-text/CellText.vue
@@ -0,0 +1,35 @@
+<!-- v1.0 -->
+<template>
+  <td class="cell-text">
+    <div class="data typo p2-regular">
+      <slot />
+      <span class="info typo p4-regular">
+        <slot name="secondary-label" />
+      </span>
+    </div>
+  </td>
+</template>
+
+<script lang="ts" setup>
+defineSlots<{
+  default: () => any
+  'secondary-label': () => any
+}>()
+</script>
+
+<style lang="postcss" scoped>
+.cell-text {
+  padding: 0.8rem;
+  border: 0.1rem solid var(--color-grey-500);
+}
+
+.data {
+  display: flex;
+  gap: 1.6rem;
+  align-items: center;
+}
+
+.info {
+  color: var(--color-grey-300);
+}
+</style>
